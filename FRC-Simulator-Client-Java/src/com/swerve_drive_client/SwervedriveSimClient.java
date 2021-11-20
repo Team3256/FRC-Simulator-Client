94af962e41c580ad2f1a6bb99563cc8fb0c57595
3LucasZ <lucas.j.zheng@gmail.com>
2021-11-20 00:05:40 -0800

    V 1.0.1
    Cleaner API!
    - connect method
    - remove exception throws
    - fixed global/private attribute problems

--- FRC-Simulator-Client-Java/src/com/swerve_drive_client/SwervedriveSimClient.java
+++ FRC-Simulator-Client-Java/src/com/swerve_drive_client/SwervedriveSimClient.java
@@ -13,22 +13,45 @@ public class SwervedriveSimClient {
     private int port;
     public RobotData data;
     private ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    private boolean connected = false;
 
-    public SwervedriveSimClient(String simAddress, int simPort) throws SocketException, UnknownHostException {
-        socket = new DatagramSocket(5000);
-        address = InetAddress.getByName(simAddress);
-        port = simPort;
+    public SwervedriveSimClient() {
         data = new RobotData();
     }
-    public void sendData() throws IOException {
-        String json = ow.writeValueAsString(data);
-        byte[] toSend = json.getBytes();
-        DatagramPacket packet = new DatagramPacket(toSend, toSend.length, address, port);
-        socket.send(packet);
+    public void connect(String simAddress, int simPort) {
+        try {
+            socket = new DatagramSocket(5000);
+            address = InetAddress.getByName(simAddress);
+            port = simPort;
+            connected = true;
+        }
+        catch (SocketException e) {
+            System.out.println("Failed to connect:");
+            System.out.println(e);
+        }
+        catch (UnknownHostException e) {
+            System.out.println("Failed to connect:");
+            System.out.println(e);
+        }
+
     }
-    public void checkData() throws JsonProcessingException {
-        String json = ow.writeValueAsString(data);
-        System.out.println(json);
+    public void sendData() throws IOException {
+        if (connected) {
+            String json = ow.writeValueAsString(data);
+            byte[] toSend = json.getBytes();
+            DatagramPacket packet = new DatagramPacket(toSend, toSend.length, address, port);
+            socket.send(packet);
+        }
+    }
+    public void checkData() {
+        try {
+            String json = ow.writeValueAsString(data);
+            System.out.println(json);
+        }
+        catch (JsonProcessingException e) {
+            System.out.println("Failed to convert data to json:");
+            System.out.println(e);
+        }
     }
     public void close() {
         socket.close();
